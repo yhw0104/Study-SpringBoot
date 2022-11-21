282f8c1147e663602986e59993fbe1b4067c06e3
yhw0104 <yhw0104@naver.com>
2022-11-21 12:01:44 +0900

    6th commit (List in view)

--- src/main/java/com/example/springstudy/controller/articleController.java
+++ src/main/java/com/example/springstudy/controller/articleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @Slf4j  //로깅을 위한 어노테이션 로깅이란 서버에서 일어나는 일들을 다 기록하는 것
 public class articleController {
@@ -51,5 +53,13 @@ public class articleController {
         // 3. 보여줄 페이지를 설정!
         return "articles/show";
     }
-
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 Article을 가져온다
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2. 가져온 Article 묶음을 뷰로 전달
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰페이지 설정
+        return "articles/index";    //articles/index.mustache
+    }
 }
--- src/main/java/com/example/springstudy/repository/ArticleRepository.java
+++ src/main/java/com/example/springstudy/repository/ArticleRepository.java
@@ -3,8 +3,12 @@ package com.example.springstudy.repository;
 import com.example.springstudy.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 // Repository는 입력 받은 데이터들의 처리를 담당한다.
 public interface ArticleRepository extends CrudRepository <Article, Long> {
     //관리대상 entity, 관리대상 entity의 대표값 타입을 파라미터에 넣어줘야된다.
     // CrudRepository를 상속 받음으로서 CRUD의 기본동작을 추가 코드 구현 없이 사용을 가능하게 한다.
+    @Override
+    ArrayList<Article> findAll();
 }
