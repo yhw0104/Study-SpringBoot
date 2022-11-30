a135c6ad44c4ef98ac2840d8246aa5c76e69de5e
yhw0104 <yhw0104@naver.com>
2022-11-30 11:26:49 +0900

    13th commit (JPA logging)

--- src/main/java/com/example/springstudy/entity/Article.java
+++ src/main/java/com/example/springstudy/entity/Article.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 Entity로 인식하게 하는 어노테이션
+@Entity // DB가 해당 객체를 Entity로 인식하게 하는 어노테이션 -> 해당 클래스로 테이블 생성 --> create table article() 쿼리문 자동 생성
+// DB의 테이블과 일대일로 매칭되는 객체 단위
+// spring.jpa.hibernate.ddl-auto 설정이 create 혹은 update로 되어있을 경우 spring 프로젝트가 시작될 때 EntityManager가 자동으로 DDL을 수행해 테이블을 생성함
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -18,7 +17,7 @@ import javax.persistence.Id;
 public class Article {
 
     @Id     // PK(Primary Key)기본키 지정
-    @GeneratedValue     // 새로운 레코드가 생성될 때마다 자동으로 PK에 +1을 해주는 어노테이션
+    @GeneratedValue (strategy = GenerationType.IDENTITY)    // -> 데이터베이스가 알아서 id를 자동으로 생성하게 함(더미데이터가 있어도 자동으로 다음 id로 생성)    // DB가 id를 자동생성해주는 어노테이션(새로운 레코드가 생성될 때마다 자동으로 PK에 +1을 해주는 어노테이션)
     private Long id;
 
     @Column     // DB 테이블에 연결해 주는 어노테이션
